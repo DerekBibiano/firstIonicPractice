fc454fb78d5d3406e45010169216d3c8bc2c298a
DerekRodBibRdz <bibianorodriguezderekrodrigo@gmail.com>
2024-09-13 01:38:26 -0600

    FinishTakePhoto
    The app can take photos

--- appCamera/src/pages/Tab2.tsx
+++ appCamera/src/pages/Tab2.tsx
@@ -1,9 +1,12 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg, IonActionSheet, } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
 import { camera, trash, close } from 'ionicons/icons';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+    const { takePhoto } = usePhotoGallery();
+    // snip - rest of code
   return (
     <IonPage>
       <IonHeader>
